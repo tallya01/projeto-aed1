2a1aedbed07f120bdc0a617115d0e5da40c506fb
Tallya Barbosa <68134534+tallya01@users.noreply.github.com>
2021-03-15 21:40:06 -0300

    Func. emprestar livros / nova visualização
    Corrigir parâmetros da função compare_date (deve passar somente um ponteiro para struct do tipo Emprestimo)

--- back.h
+++ back.h
@@ -14,6 +14,7 @@ typedef struct{
     int genre;
     int ano_publicacao;
     int quantidade_exemplares;
+    int exemplares_disponiveis;
 } Livro;
 
 //estrutura para data
@@ -29,10 +30,19 @@ typedef struct{
     int mm; // minuto
 } Hora;
 
+typedef struct{
+    char nome_livro[100];
+    char nome_pessoa[100];
+    Hora hora_retirada;
+    Data data_retirada;
+    Data data_devolucao;
+} Emprestimo;
+
 //declaração das estruturas
 Livro livro;
 Data data;
 Hora hora;
+Emprestimo emprestimo;
 
 /**
  * @brief adiciona um novo livro
@@ -40,8 +50,34 @@ Hora hora;
  * @param win janela da qual será coletada a entrada do usuário e 
  * na qual serão impressas as mensagens necessárias
  */
-void add_livro(WINDOW * menu);
+void add_book(WINDOW * menu);
 
-void print_books(WINDOW * menu, int choice);
+/**
+ * @brief Busca e imprime livros
+ * 
+ * @param menu janela
+ * @param choice modo de operação da função
+ */
+void search_and_print_books(WINDOW * menu, int choice);
+
+void lend_book(Livro * book, WINDOW * win);
+
+/**
+ * @brief Compara se a data e hora inseridas pelo usuário com as atuais
+ * 
+ * @param dd dia
+ * @param mm mês
+ * @param yy ano
+ * @return int 0 se estiver no passado, 1 se não
+ */
+int compare_date(int dd, int mm, int yy); //adaptar parâmetros
+
+void return_book_date(Emprestimo * lend);
+
+int leap_year(int ano);
+
+int number_of_days(int mes, int ano);
+
+void see_lent_books(WINDOW * win);
 
 #endif
