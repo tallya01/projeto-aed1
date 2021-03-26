74a7d348e247caf136299110de85a90f17eb3785
Tallya Barbosa <68134534+tallya01@users.noreply.github.com>
2021-03-26 09:41:10 -0300

    Biblioteca ordenada e documentada

--- lib.h
+++ lib.h
@@ -52,14 +52,38 @@ Hora hora;
 Emprestimo emprestimo;
 
 /**
- * @brief função para imprimir um menu na tela
+ * @brief adiciona um novo livro
  * 
- * @param menu vetor contendo as strings das opções do menu 
- * @param menuSize número de opções do menu
- * @param win janela na qual o menu deverá ser impresso
- * @param startY linha da tela na qual a função deve iniciar
+ * @param win janela da qual será coletada a entrada do usuário e 
+ * na qual serão impressas as mensagens necessárias
  */
-void print_menu(char ** menu, int menuSize, WINDOW * win, int startY);
+void add_book(WINDOW * menu);
+
+/**
+ * @brief Verifica se a data passada como parâmetro está
+ * no passado ou não
+ * 
+ * @param data 
+ * @return 0 se sim, 1 se não
+ */
+int compare_date(Data * data);
+
+/**
+ * @brief Calcula a quantidade de dias passados
+ * a partir de uma data
+ * 
+ * @param data
+ * @return Quantidade de dias 
+ */
+int days_late(Data * data);
+
+/**
+ * @brief Deleta o registro de um livro
+ * 
+ * @param book 
+ * @param win 
+ */
+void delete_book(Livro * book, WINDOW * win);
 
 /**
  * @brief função para retornar o valor da opção escolhida pelo usuário
@@ -75,41 +99,12 @@ void print_menu(char ** menu, int menuSize, WINDOW * win, int startY);
 int get_choice(char ** menu, int menuSize, int mouseX, int mouseY, int startY);
 
 /**
- * @brief adiciona um novo livro
+ * @brief Retorna o gênero de acordo com o código
  * 
- * @param win janela da qual será coletada a entrada do usuário e 
- * na qual serão impressas as mensagens necessárias
+ * @param genre 
+ * @return char* 
  */
-void add_book(WINDOW * menu);
-
-/**
- * @brief Busca e imprime livros
- * 
- * @param menu janela
- * @param choice modo de operação da função
- */
-void search_and_print_books(WINDOW * menu, int choice);
-
-void lend_book(Livro * book, WINDOW * win);
-
-/**
- * @brief Verifica se a data passada como parâmetro está
- * no passado ou não
- * 
- * @param data 
- * @return 0 se sim, 1 se não
- */
-int compare_date(Data * data);
-
-int days_late(Data * data);
-
-/**
- * @brief Retorna a data de devolução de um livro
- * (7 dias a partir da data atual)
- * 
- * @param lend struct do tipo Emprestimo
- */
-void ret_book_date(Emprestimo * lend);
+char *get_genre(int genre);
 
 /**
  * @brief Verifica se um ano é bissexto
@@ -120,6 +115,14 @@ void ret_book_date(Emprestimo * lend);
 int leap_year(int ano);
 
 /**
+ * @brief Empresta um livro
+ * 
+ * @param book 
+ * @param win 
+ */
+void lend_book(Livro * book, WINDOW * win);
+
+/**
  * @brief Retona o número de dias de um mês
  * 
  * @param mes 
@@ -127,6 +130,58 @@ int leap_year(int ano);
  * @return int 
  */
 int number_of_days(int mes, int ano);
+
+/**
+ * @brief ordena os livros de acordo com a escolha do user
+ * 
+ * @param book 
+ * @param tamanho 
+ * @param choice 
+ */
+
+void order_books(Livro *book, int tamanho, int choice);
+
+/**
+ * @brief função para imprimir um menu na tela
+ * 
+ * @param menu vetor contendo as strings das opções do menu 
+ * @param menuSize número de opções do menu
+ * @param win janela na qual o menu deverá ser impresso
+ * @param startY linha da tela na qual a função deve iniciar
+ */
+void print_menu(char ** menu, int menuSize, WINDOW * win, int startY);
+
+/**
+ * @brief Renova o empréstimo de um livro
+ * 
+ * @param lending 
+ * @param win 
+ */
+void renew_book(Emprestimo * lending, WINDOW * win);
+
+/**
+ * @brief Registra a devolução de um livro
+ * 
+ * @param lending 
+ * @param win 
+ */
+void ret_book(Emprestimo * lending, WINDOW * win);
+
+/**
+ * @brief Retorna a data de devolução de um livro
+ * (7 dias a partir da data atual)
+ * 
+ * @param lend struct do tipo Emprestimo
+ */
+void ret_book_date(Emprestimo * lend);
+
+/**
+ * @brief Busca e imprime livros
+ * 
+ * @param menu janela
+ * @param choice modo de operação da função
+ */
+void search_and_print_books(WINDOW * menu, int choice);
 
 /**
  * @brief Imprime na tela os livros que estão emprestados 
@@ -142,25 +197,14 @@ void see_lent_books(WINDOW * win);
  * @param i posição 1
  * @param j posição 2
  */
-
 void swap_livros(Livro *book, int i, int j);
 
 /**
- * @brief ordena os livros de acordo com a escolha do user
+ * @brief Verifica se um livro já está cadastrado no sistema
  * 
- * @param book 
- * @param tamanho 
- * @param choice 
+ * @param nomeLivro 
+ * @return int 1 se sim, 0 se não
  */
-
-void order_books(Livro *book, int tamanho, int choice);
-
-void renew_book(Emprestimo * lending, WINDOW * win);
-
-void ret_book(Emprestimo * lending, WINDOW * win);
-
-void delete_book(Livro * book, WINDOW * win);
-
 int verify_book(char *nomeLivro);
 
 #endif
