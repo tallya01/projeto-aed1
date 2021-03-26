17d2202c6660173dcd975e23b4fb695dbf1d48cb
Tallya Barbosa <68134534+tallya01@users.noreply.github.com>
2021-03-26 09:41:36 -0300

    Update lib.c

--- lib.c
+++ lib.c
@@ -18,26 +18,6 @@ char * ordenacao_menu[3] = {
     "3. Ano de Publicação",
 };
 
-void print_menu(char ** menu, int menuSize, WINDOW * win, int startY){
-    int i;
-
-    for(i=0; i<menuSize; i++, startY++){
-        mvwaddstr(win, startY, 1, menu[i]);
-    }
-
-    wrefresh(win);
-}
-
-int get_choice(char ** menu, int menuSize, int mouseX, int mouseY, int startY){
-    int i;
-
-    for(i=0;i<menuSize;i++){
-        if(mouseX>=1 && mouseX<=strlen(menu[i])+1 && mouseY == startY+i) return i+49;
-    }
-
-    return -1;
-}
-
 void add_book(WINDOW * menu){
     wclear(menu);
     box(menu,0,0);
@@ -116,6 +96,87 @@ verify_mouse_genre_entry:
     getch();
 }
 
+int compare_date(Data * data){
+    time_t rawtime;
+    struct tm * date;
+    time(&rawtime);
+    date = localtime(&rawtime); //coleta a data e hora atuais
+
+    if(data->yy<date->tm_year+1900) return 0;
+    else if(data->yy>date->tm_year+1900) return 1;
+    else if(data->mm<date->tm_mon+1) return 0;
+    else if(data->mm>date->tm_mon+1) return 1;
+    else if(data->dd<date->tm_mday) return 0;
+    else if(data->dd>date->tm_mday) return 1;
+
+    return 1;
+}
+
+int days_late(Data * data){
+    time_t rawtime;
+    struct tm * date;
+    time(&rawtime);
+    date = localtime(&rawtime);
+    int days = 0, m, y;
+
+    if(data->yy==date->tm_year+1900 && data->mm==date->tm_mon+1){
+        days = date->tm_mday-data->dd;
+    }
+    else if(data->yy==date->tm_year+1900){
+        days += (number_of_days(data->mm, data->yy)-data->dd);
+        for(m=data->mm+1; m<date->tm_mon+1; m++) days += number_of_days(m, data->yy);
+        days += date->tm_mday;
+    }
+    else{
+        for(y=data->yy; y<=date->tm_year+1900; y++){
+            if(y==data->yy){
+                days += (number_of_days(data->mm, y)-data->dd);
+                for(m=data->mm+1; m<=12; m++) days += number_of_days(m, y);
+            }
+            else if(y!=date->tm_year+1900){
+                for(m=1;m<=12;m++) days += number_of_days(m, y);
+            }
+            else days += (date->tm_yday+1);
+        }
+    }
+
+    return days;
+}
+
+void delete_book(Livro * book, WINDOW * win){
+    FILE * arq = fopen("books.dat", "rb"), * temp = fopen("temp.dat", "ab");
+    Livro bk;
+
+    wclear(win);
+    box(win,0,0);
+
+    while(fread(&bk, sizeof(Livro), 1, arq)){
+        if(strcmp(bk.nome_livro, book->nome_livro)==0){
+            mvwaddstr(win,1,1,"Livro deletado com sucesso!");
+            mvwaddstr(win,3,1,"Pressione qualquer tecla para voltar");
+        }
+        else fwrite(&bk, sizeof(Livro), 1, temp);
+    }
+
+    fclose(arq);
+    fclose(temp);
+    remove("books.dat");
+    rename("temp.dat", "books.dat");
+    wrefresh(win);
+    getch();
+    return;
+}
+
+int get_choice(char ** menu, int menuSize, int mouseX, int mouseY, int startY){
+    int i;
+
+    for(i=0;i<menuSize;i++){
+        if(mouseX>=1 && mouseX<=strlen(menu[i])+1 && mouseY == startY+i) return i+49;
+    }
+
+    return -1;
+}
+
 char *get_genre(int genre){
 
     switch(genre){
@@ -128,28 +189,93 @@ char *get_genre(int genre){
     }
 }
 
-int verify_book(char *nomeLivro){
-    FILE *file = fopen("books.dat", "rb");
-    if(file == NULL) return 0;
-    int count = 0;
-    Livro temp;
-
-    while(fread(&temp, sizeof(Livro), 1, file)==1){
-        if(strcmp(temp.nome_livro, nomeLivro) == 0) count++;
-    }
-    fclose(file);
-    if(count>0) return 1;
-    else return 0;
+int leap_year(int ano){
+    if(ano % 400 == 0 || (ano % 100 != 0 && ano % 4 == 0))
+    return 1; // ano bissexto
+    return 0; // ano não bissexto
 }
 
-void swap_livros(Livro *book, int i, int j){
-    Livro *temp = (Livro*) malloc(sizeof(Livro));
-    
-    temp[0] = book[i];
-    book[i] = book[j];
-    book[j] = temp[0];
+void lend_book(Livro * book, WINDOW * win){
+    Emprestimo lending;
+    Livro li;
+    FILE * arq = fopen("lent.dat", "ab+");
+    FILE * livro = fopen("books.dat", "rb+");
+    time_t rawtime;
+    struct tm * date;
+    int i;
+    time(&rawtime);
+    date = localtime(&rawtime);
 
-    free(temp);
+    wclear(win);
+    box(win,0,0);
+
+    if(book->exemplares_disponiveis==0){
+        mvwaddstr(win, 1,1, "Não há exemplares disponíveis desse livro!");
+        fclose(arq);
+        fclose(livro);
+        wrefresh(win);
+        getch();
+        return;
+    }
+
+    strcpy(lending.nome_livro, book->nome_livro);
+
+    //atualiza a quantidade de exemplares disponíveis no books.dat
+    while(fread(&li, sizeof(Livro), 1, livro)==1){
+        if(strcmp(li.nome_livro, lending.nome_livro)==0){
+            fseek(livro, (-1)*sizeof(Livro), SEEK_CUR);
+            --li.exemplares_disponiveis;
+            fwrite(&li, sizeof(Livro), 1, livro);
+            fclose(livro);
+            break;
+        }
+    }
+    
+    mvwaddstr(win, 1,1, "Digite o nome da pessoa que está retirando o livro:");
+    curs_set(1);
+    echo();
+    wrefresh(win);
+    mvwscanw(win, 2,1, "%[^\n]", &lending.nome_pessoa);
+    curs_set(0);
+    noecho();
+    lending.data_retirada.dd = date->tm_mday;
+    lending.data_retirada.mm = date->tm_mon+1;
+    lending.data_retirada.yy = date->tm_year+1900;
+    lending.hora_retirada.hh = date->tm_hour;
+    lending.hora_retirada.mm = date->tm_min;
+    ret_book_date(&lending);
+
+    if(fwrite(&lending, sizeof(Emprestimo), 1, arq)){
+        mvwprintw(win, 4,1, "Empréstimo registrado com sucesso, data de devolução %d/%d/%d", lending.data_devolucao.dd, lending.data_devolucao.mm, lending.data_devolucao.yy);
+        wrefresh(win);
+        fclose(arq);
+        getch();
+    }
+    else{
+        mvwaddstr(win, 4,1, "Empréstimo mal sucedido, tente novamente");
+        wrefresh(win);
+        fclose(arq);
+        getch();
+    }
+}
+
+int number_of_days(int mes, int ano){
+    switch(mes){
+        case 1: return 31;
+        case 2: if(leap_year(ano)==1) return 29;
+            else return 28;
+        case 3: return 31;
+        case 4: return 30;
+        case 5: return 31;
+        case 6: return 30;
+        case 7: return 31;
+        case 8: return 31;
+        case 9: return 30;
+        case 10: return 31;
+        case 11: return 30;
+        case 12: return 31; //é natal é natal ... PEGA NO MEU PÉ
+        default: return -1; //usuário inseriu um mês inválido 
+    }
 }
 
 void order_books(Livro *book, int tamanho, int choice){
@@ -184,6 +310,109 @@ void order_books(Livro *book, int tamanho, int choice){
                 }
             }
         break;
+    }
+}
+
+void print_menu(char ** menu, int menuSize, WINDOW * win, int startY){
+    int i;
+
+    for(i=0; i<menuSize; i++, startY++){
+        mvwaddstr(win, startY, 1, menu[i]);
+    }
+
+    wrefresh(win);
+}
+
+void renew_book(Emprestimo * lending, WINDOW * win){
+    FILE * arq = fopen("lent.dat", "rb+");
+    int i = 5;
+
+    wclear(win);
+    box(win,0,0);
+
+    while(fread(&emprestimo, sizeof(Emprestimo), 1, arq)){
+        if(strcmp(emprestimo.nome_livro,lending->nome_livro)==0 && strcmp(emprestimo.nome_pessoa,lending->nome_pessoa)==0){
+            ret_book_date(&emprestimo);
+            fseek(arq, (-1)*sizeof(Emprestimo), SEEK_CUR);
+            fwrite(&emprestimo, sizeof(Emprestimo), 1, arq);
+            fclose(arq);
+            mvwprintw(win, 1,1,"Renovação registrada! Nova data de devolução: %d/%d/%d", emprestimo.data_devolucao.dd, emprestimo.data_devolucao.mm, emprestimo.data_devolucao.yy);
+            mvwaddstr(win, 3,1, "Pressione qualquer tecla para voltar.");
+            wrefresh(win);
+            getch();
+            return;
+        }
+    }
+
+    fclose(arq);
+    mvwaddstr(win, 1,1,"Não foi possível registrar a devolução! Tente novamente.");
+    mvwaddstr(win, 3,1, "Pressione qualquer tecla para voltar.");
+    wrefresh(win);
+    getch();
+    return;
+}
+
+void ret_book(Emprestimo * lending, WINDOW * win){
+    FILE * li = fopen("books.dat", "rb+"), * lend = fopen("lent.dat", "rb"), * temp = fopen("temp.dat", "ab");
+    Livro bk;
+    int i=0;
+
+    wclear(win);
+    box(win,0,0);
+
+    //atualiza a quantidade de exemplares disponíveis
+    while(fread(&bk, sizeof(Livro), 1, li)){
+        if(strcmp(bk.nome_livro, lending->nome_livro)==0){
+            ++bk.exemplares_disponiveis;
+            fseek(li, (-1)*sizeof(Livro), SEEK_CUR);
+            fwrite(&bk, sizeof(Livro), 1, li);
+            fclose(li);
+            break;
+        }
+    }
+
+    //deleta o livro devolvido do lent.dat
+    while(fread(&emprestimo, sizeof(Emprestimo), 1, lend)){
+        if(strcmp(emprestimo.nome_pessoa, lending->nome_pessoa)==0 && strcmp(emprestimo.nome_livro, lending->nome_livro)==0){
+            mvwaddstr(win, 1,1, "Devolução registrada!");
+            mvwaddstr(win, 3,1, "Pressione qualquer tecla para voltar");
+            wrefresh(win);
+        }
+        else{
+            fwrite(&emprestimo, sizeof(Emprestimo), 1, temp);
+        }
+    }
+
+    fclose(temp);
+    fclose(lend);
+    remove("lent.dat");
+    rename("temp.dat", "lent.dat");
+
+    getch();
+    return;
+}
+
+void ret_book_date(Emprestimo * lend){
+    time_t rawtime;
+    struct tm * date;
+    time(&rawtime);
+    date = localtime(&rawtime);
+
+    if(date->tm_mday+7>number_of_days(date->tm_mon+1, date->tm_year+1900)){
+        if(date->tm_mon+1>12){
+            lend->data_devolucao.yy = date->tm_year+1901;
+            lend->data_devolucao.mm = 1;
+        } 
+        else{
+            lend->data_devolucao.yy = date->tm_year+1900;
+            lend->data_devolucao.mm = date->tm_mon+2;
+        }
+        lend->data_devolucao.dd = (date->tm_mday+7)-number_of_days(date->tm_mon+1, date->tm_year+1900);
+    }
+    else{
+        lend->data_devolucao.dd = date->tm_mday+7;
+        lend->data_devolucao.mm = date->tm_mon+1;
+        lend->data_devolucao.yy = date->tm_year+1900;
     }
 }
 
@@ -542,71 +771,6 @@ verify_mouse_ordenacao_entry:
     }
 }
 
-void lend_book(Livro * book, WINDOW * win){
-    Emprestimo lending;
-    Livro li;
-    FILE * arq = fopen("lent.dat", "ab+");
-    FILE * livro = fopen("books.dat", "rb+");
-    time_t rawtime;
-    struct tm * date;
-    int i;
-    time(&rawtime);
-    date = localtime(&rawtime);
-
-    wclear(win);
-    box(win,0,0);
-
-    if(book->exemplares_disponiveis==0){
-        mvwaddstr(win, 1,1, "Não há exemplares disponíveis desse livro!");
-        fclose(arq);
-        fclose(livro);
-        wrefresh(win);
-        getch();
-        return;
-    }
-
-    strcpy(lending.nome_livro, book->nome_livro);
-
-    //atualiza a quantidade de exemplares disponíveis no books.dat
-    while(fread(&li, sizeof(Livro), 1, livro)==1){
-        if(strcmp(li.nome_livro, lending.nome_livro)==0){
-            fseek(livro, (-1)*sizeof(Livro), SEEK_CUR);
-            --li.exemplares_disponiveis;
-            fwrite(&li, sizeof(Livro), 1, livro);
-            fclose(livro);
-            break;
-        }
-    }
-    
-    mvwaddstr(win, 1,1, "Digite o nome da pessoa que está retirando o livro:");
-    curs_set(1);
-    echo();
-    wrefresh(win);
-    mvwscanw(win, 2,1, "%[^\n]", &lending.nome_pessoa);
-    curs_set(0);
-    noecho();
-    lending.data_retirada.dd = date->tm_mday;
-    lending.data_retirada.mm = date->tm_mon+1;
-    lending.data_retirada.yy = date->tm_year+1900;
-    lending.hora_retirada.hh = date->tm_hour;
-    lending.hora_retirada.mm = date->tm_min;
-    ret_book_date(&lending);
-
-    if(fwrite(&lending, sizeof(Emprestimo), 1, arq)){
-        mvwprintw(win, 4,1, "Empréstimo registrado com sucesso, data de devolução %d/%d/%d", lending.data_devolucao.dd, lending.data_devolucao.mm, lending.data_devolucao.yy);
-        wrefresh(win);
-        fclose(arq);
-        getch();
-    }
-    else{
-        mvwaddstr(win, 4,1, "Empréstimo mal sucedido, tente novamente");
-        wrefresh(win);
-        fclose(arq);
-        getch();
-    }
-
-}
-
 void see_lent_books(WINDOW * win){
     FILE * arq = fopen("lent.dat", "rb");
     int i = 1, k = 0, action = 0;
@@ -683,191 +847,26 @@ void see_lent_books(WINDOW * win){
     fclose(arq);
 }
 
-void renew_book(Emprestimo * lending, WINDOW * win){
-    FILE * arq = fopen("lent.dat", "rb+");
-    int i = 5;
+void swap_livros(Livro *book, int i, int j){
+    Livro *temp = (Livro*) malloc(sizeof(Livro));
+    
+    temp[0] = book[i];
+    book[i] = book[j];
+    book[j] = temp[0];
 
-    wclear(win);
-    box(win,0,0);
-
-    while(fread(&emprestimo, sizeof(Emprestimo), 1, arq)){
-        if(strcmp(emprestimo.nome_livro,lending->nome_livro)==0 && strcmp(emprestimo.nome_pessoa,lending->nome_pessoa)==0){
-            ret_book_date(&emprestimo);
-            fseek(arq, (-1)*sizeof(Emprestimo), SEEK_CUR);
-            fwrite(&emprestimo, sizeof(Emprestimo), 1, arq);
-            fclose(arq);
-            mvwprintw(win, 1,1,"Renovação registrada! Nova data de devolução: %d/%d/%d", emprestimo.data_devolucao.dd, emprestimo.data_devolucao.mm, emprestimo.data_devolucao.yy);
-            mvwaddstr(win, 3,1, "Pressione qualquer tecla para voltar.");
-            wrefresh(win);
-            getch();
-            return;
-        }
-    }
-
-    fclose(arq);
-    mvwaddstr(win, 1,1,"Não foi possível registrar a devolução! Tente novamente.");
-    mvwaddstr(win, 3,1, "Pressione qualquer tecla para voltar.");
-    wrefresh(win);
-    getch();
-    return;
+    free(temp);
 }
 
-void ret_book(Emprestimo * lending, WINDOW * win){
-    FILE * li = fopen("books.dat", "rb+"), * lend = fopen("lent.dat", "rb"), * temp = fopen("temp.dat", "ab");
-    Livro bk;
-    int i=0;
+int verify_book(char *nomeLivro){
+    FILE *file = fopen("books.dat", "rb");
+    if(file == NULL) return 0;
+    int count = 0;
+    Livro temp;
 
-    wclear(win);
-    box(win,0,0);
-
-    //atualiza a quantidade de exemplares disponíveis
-    while(fread(&bk, sizeof(Livro), 1, li)){
-        if(strcmp(bk.nome_livro, lending->nome_livro)==0){
-            ++bk.exemplares_disponiveis;
-            fseek(li, (-1)*sizeof(Livro), SEEK_CUR);
-            fwrite(&bk, sizeof(Livro), 1, li);
-            fclose(li);
-            break;
-        }
+    while(fread(&temp, sizeof(Livro), 1, file)==1){
+        if(strcmp(temp.nome_livro, nomeLivro) == 0) count++;
     }
-
-    //deleta o livro devolvido do lent.dat
-    while(fread(&emprestimo, sizeof(Emprestimo), 1, lend)){
-        if(strcmp(emprestimo.nome_pessoa, lending->nome_pessoa)==0 && strcmp(emprestimo.nome_livro, lending->nome_livro)==0){
-            mvwaddstr(win, 1,1, "Devolução registrada!");
-            mvwaddstr(win, 3,1, "Pressione qualquer tecla para voltar");
-            wrefresh(win);
-        }
-        else{
-            fwrite(&emprestimo, sizeof(Emprestimo), 1, temp);
-        }
-    }
-
-    fclose(temp);
-    fclose(lend);
-    remove("lent.dat");
-    rename("temp.dat", "lent.dat");
-
-    getch();
-    return;
-}
-
-void delete_book(Livro * book, WINDOW * win){
-    FILE * arq = fopen("books.dat", "rb"), * temp = fopen("temp.dat", "ab");
-    Livro bk;
-
-    wclear(win);
-    box(win,0,0);
-
-    while(fread(&bk, sizeof(Livro), 1, arq)){
-        if(strcmp(bk.nome_livro, book->nome_livro)==0){
-            mvwaddstr(win,1,1,"Livro deletado com sucesso!");
-            mvwaddstr(win,3,1,"Pressione qualquer tecla para voltar");
-        }
-        else fwrite(&bk, sizeof(Livro), 1, temp);
-    }
-
-    fclose(arq);
-    fclose(temp);
-    remove("books.dat");
-    rename("temp.dat", "books.dat");
-    wrefresh(win);
-    getch();
-    return;
-}
-
-int compare_date(Data * data){
-    time_t rawtime;
-    struct tm * date;
-    time(&rawtime);
-    date = localtime(&rawtime); //coleta a data e hora atuais
-
-    if(data->yy<date->tm_year+1900) return 0;
-    else if(data->yy>date->tm_year+1900) return 1;
-    else if(data->mm<date->tm_mon+1) return 0;
-    else if(data->mm>date->tm_mon+1) return 1;
-    else if(data->dd<date->tm_mday) return 0;
-    else if(data->dd>date->tm_mday) return 1;
-
-    return 1;
-}
-
-int days_late(Data * data){
-    time_t rawtime;
-    struct tm * date;
-    time(&rawtime);
-    date = localtime(&rawtime);
-    int days = 0, m, y;
-
-    if(data->yy==date->tm_year+1900 && data->mm==date->tm_mon+1){
-        days = date->tm_mday-data->dd;
-    }
-    else if(data->yy==date->tm_year+1900){
-        days += (number_of_days(data->mm, data->yy)-data->dd);
-        for(m=data->mm+1; m<date->tm_mon+1; m++) days += number_of_days(m, data->yy);
-        days += date->tm_mday;
-    }
-    else{
-        for(y=data->yy; y<=date->tm_year+1900; y++){
-            if(y==data->yy){
-                days += (number_of_days(data->mm, y)-data->dd);
-                for(m=data->mm+1; m<=12; m++) days += number_of_days(m, y);
-            }
-            else if(y!=date->tm_year+1900){
-                for(m=1;m<=12;m++) days += number_of_days(m, y);
-            }
-            else days += (date->tm_yday+1);
-        }
-    }
-
-    return days;
-}
-
-void ret_book_date(Emprestimo * lend){
-    time_t rawtime;
-    struct tm * date;
-    time(&rawtime);
-    date = localtime(&rawtime);
-
-    if(date->tm_mday+7>number_of_days(date->tm_mon+1, date->tm_year+1900)){
-        if(date->tm_mon+1>12){
-            lend->data_devolucao.yy = date->tm_year+1901;
-            lend->data_devolucao.mm = 1;
-        } 
-        else{
-            lend->data_devolucao.yy = date->tm_year+1900;
-            lend->data_devolucao.mm = date->tm_mon+2;
-        }
-        lend->data_devolucao.dd = (date->tm_mday+7)-number_of_days(date->tm_mon+1, date->tm_year+1900);
-    }
-    else{
-        lend->data_devolucao.dd = date->tm_mday+7;
-        lend->data_devolucao.mm = date->tm_mon+1;
-        lend->data_devolucao.yy = date->tm_year+1900;
-    }
-}
-
-int leap_year(int ano){
-    if(ano % 400 == 0 || (ano % 100 != 0 && ano % 4 == 0))
-    return 1; // ano bissexto
-    return 0; // ano não bissexto
-}
-
-int number_of_days(int mes, int ano){
-    switch(mes){
-        case 1: return 31;
-        case 2: if(leap_year(ano)==1) return 29;
-            else return 28;
-        case 3: return 31;
-        case 4: return 30;
-        case 5: return 31;
-        case 6: return 30;
-        case 7: return 31;
-        case 8: return 31;
-        case 9: return 30;
-        case 10: return 31;
-        case 11: return 30;
-        case 12: return 31; //é natal é natal ... PEGA NO MEU PÉ
-        default: return -1; //usuário inseriu um mês inválido 
-    }
+    fclose(file);
+    if(count>0) return 1;
+    else return 0;
 }
